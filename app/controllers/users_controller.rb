22ceb320eac0011a71d34cee280a5375e95809e2
Christopher Kim <christopherjkim@gmail.com>
2011-12-20 00:54:25 -0500

    Re-enabled CSRF protection and cert verification
    see: http://stackoverflow.com/a/5271761

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   
+  skip_before_filter :verify_authenticity_token
   before_filter :can_edit, :only => [:edit, :update, :destroy, :confirm_delete]
   before_filter :require_admin, :only => [:admin, :ban, :remove_ban]
   skip_filter :check_privacy, :only => [:login, :logout]
