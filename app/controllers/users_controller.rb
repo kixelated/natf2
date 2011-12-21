8953d1c53970f34878cc5ec003219296a1772256
Christopher Kim <christopherjkim@gmail.com>
2011-12-21 18:21:26 -0500

    Created omni_controller to handle steam auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   
-  skip_before_filter :verify_authenticity_token
   before_filter :can_edit, :only => [:edit, :update, :destroy, :confirm_delete]
   before_filter :require_admin, :only => [:admin, :ban, :remove_ban]
   skip_filter :check_privacy, :only => [:login, :logout]
@@ -86,24 +85,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def steam_login
-    @user = User.find_by_id(session[:user_id])
-    if @user
-      if @user.steamid.blank?
-        @user.steamid = auth_hash.uid
-        flash[:notice] = "That steamid has already been taken." unless @user.save
-      else
-        flash[:notice] = "You cannot change your steamid."
-      end
-    end
-    redirect_to edit_user_path(@user)
-  end
-  
-  def steam_login_failure
-    redirect_to current_user
-    flash[:notice] = "Could not log you in. #{params[:message]}"
-  end
-  
   def logout
     redirect_to root_path and return false unless logged_in?
     @flash = flash[:notice]
@@ -121,10 +102,6 @@ class UsersController < ApplicationController
   end
     
   protected
-  
-  def auth_hash
-    request.env['omniauth.auth']
-  end
     
   def do_login(user)
     user.logged_out = false
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ require 'omniauth-steam'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/fetchers'
-  OpenID.fetcher.ca_file = '#{Rails.root}/config/ca-bundle.crt'
+  OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
   
   provider :steam
 end
