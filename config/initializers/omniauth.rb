2e22c18685ea2dfea97dd394ec1da5b7de12af21
Christopher Kim <christopherjkim@gmail.com>
2012-01-15 19:05:57 -0500

    Created omni_controller and enabled CSRF protection

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 require 'omniauth-steam'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  require 'openid/fetchers'
+  OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
   provider :steam
 end
